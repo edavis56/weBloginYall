7e858f68328dfe575bf38cbb8f2f13bc9a156a1a
edavis56 <92826817+edavis56@users.noreply.github.com>
2022-04-01 18:49:06 -0400

    update to controllers

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const homeroutes = require("./homeroutes");
+
+router.use("/api", apiRoutes);
+router.use("/", homeroutes);
+
+module.exports = router;
