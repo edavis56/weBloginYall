ad583dcc73f2165d16fa08a402a6a893d243692c
edavis56 <92826817+edavis56@users.noreply.github.com>
2022-04-01 18:55:58 -0400

    update

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
-const apiRoutes = require("./api");
-const homeroutes = require("./homeroutes");
+const userRoutes = require("./userRoutes");
+const postRoutes = require("./postRoutes");
+const commentRoutes = require("./commentRoutes");
 
-router.use("/api", apiRoutes);
-router.use("/", homeroutes);
+router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
+router.use("/comments", commentRoutes);
 
 module.exports = router;
